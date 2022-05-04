5df6cc37fedbfaec03b20a81ff9de7c44011a305
alphaikaduki <97274482+alphaikaduki@users.noreply.github.com>
2022-05-04 23:21:13 +0900

    Beta

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace GenshinPublic
+{
+    /// <summary>
+    /// Ini ファイルの読み書きを扱うクラスです。
+    /// </summary>
+    public class IniFile
+    {
+        [DllImport("kernel32.dll")]
+        private static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetPrivateProfileInt(string lpAppName, string lpKeyName, int nDefault, string lpFileName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+        /// <summary>
+        /// Ini ファイルのファイルパスを取得、設定します。
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// インスタンスを初期化します。
+        /// </summary>
+        /// <param name="filePath">Ini ファイルのファイルパス</param>
+        public IniFile(string filePath)
+        {
+            FilePath = filePath;
+        }
+        /// <summary>
+        /// Ini ファイルから文字列を取得します。
+        /// </summary>
+        /// <param name="section">セクション名</param>
+        /// <param name="key">項目名</param>
+        /// <param name="defaultValue">値が取得できない場合の初期値</param>
+        /// <returns></returns>
+        public string GetString(string section, string key, string defaultValue = "")
+        {
+            var sb = new StringBuilder(1024);
+            var r = GetPrivateProfileString(section, key, defaultValue, sb, (uint)sb.Capacity, FilePath);
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Ini ファイルから整数を取得します。
+        /// </summary>
+        /// <param name="section">セクション名</param>
+        /// <param name="key">項目名</param>
+        /// <param name="defaultValue">値が取得できない場合の初期値</param>
+        /// <returns></returns>
+        public int GetInt(string section, string key, int defaultValue = 0)
+        {
+            return (int)GetPrivateProfileInt(section, key, defaultValue, FilePath);
+        }
+        /// <summary>
+        /// Ini ファイルに文字列を書き込みます。
+        /// </summary>
+        /// <param name="section">セクション名</param>
+        /// <param name="key">項目名</param>
+        /// <param name="value">書き込む値</param>
+        /// <returns></returns>
+        public bool WriteString(string section, string key, string value)
+        {
+            return WritePrivateProfileString(section, key, value, FilePath);
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            if(File.Exists(System.AppDomain.CurrentDomain.BaseDirectory + @"\settings.ini"))
+            {
+
+            }
+
+              else
+                    {
+                File.Create(System.AppDomain.CurrentDomain.BaseDirectory + @"\settings.ini");
+                var ini = new IniFile(System.AppDomain.CurrentDomain.BaseDirectory + @"\settings.ini");
+                ini.WriteString("Genshin", "Path", @"C:\Program Files\Genshin Impact\Genshin Impact game\GenshinImpact.exe");
+                ini.GetString("Genshin", "Senden", "True");
+
+            }
+            var ini2 = new IniFile(System.AppDomain.CurrentDomain.BaseDirectory + @"\settings.ini");
+            string senden  = ini2.GetString("Genshin", "Senden", "True");
+            if (senden == "True" || senden == "true")
+            {
+                System.Diagnostics.Process.Start("https://discord.gg/bYEV8QPUtW");
+                System.Diagnostics.Process.Start("https://github.com/alphaikaduki");
+              
+                
+            }
+           
+            InitializeComponent();
+            
+            genshinpath.Text =  ini2.GetString("Genshin", "Path", @"C:\Program Files\Genshin Impact\Genshin Impact game\GenshinImpact.exe");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+      
+        private void genshinrun_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var genshin = new System.Diagnostics.Process();
+            string path = genshinpath.Text;
+            if (path == null)
+            {
+                return;
+            }
+            var ini3 = new IniFile(System.AppDomain.CurrentDomain.BaseDirectory + @"\settings.ini");
+            genshin.StartInfo.FileName = @path;
+            ini3.WriteString("Genshin", "Path", path);
+            try
+            {
+                genshin.Start();
+
+            }
+            catch (InvalidCastException)
+            {
+                return;
+                // error
+            }
+        }
+
+        private void rogubo_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481&lang=ja-jp");
+            
+        }
+
+        private void read_Click(object sender, RoutedEventArgs e)
+        {
+            // ダイアログのインスタンスを生成
+            var dialog = new OpenFileDialog();
+
+            // ファイルの種類を設定
+            dialog.Filter = "実行ファイル (*.exe)|*.exe";
+
+            // ダイアログを表示する
+            if (dialog.ShowDialog() == true)
+            {
+                // 選択されたファイル名 (ファイルパス) をメッセージボックスに表示
+                MessageBox.Show(dialog.FileName + " が読み込まれました。");
+                genshinpath.Text = dialog.FileName;
+            }
+        }
+
+  /*      private void userdata_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void getuser_Click(object sender, RoutedEventArgs e)
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"HKEY_CURRENT_USER\Software\miHoYo\Genshin Impact");
+            var data = key.GetValue("GENERAL_DATA_h2389025596");
+              userdata.Text = (string)data;
+            key.Close();
+        }
+   */ }
+    }
